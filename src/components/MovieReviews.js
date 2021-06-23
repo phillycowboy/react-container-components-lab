8decd47c6b9761c7a32e8498b50efb4635847ea7
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-23 15:51:06 -0400

    commit tree 0c9119b9ced92cb93875ac78ec48425acb7fbed0

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -6,7 +6,16 @@ const MovieReviews = (props) => {
     return(
         <div className="review-list">
             {props.reviews.map((review) => {
-                <h1>{review.display_title}</h1>
+                // console.log(review)
+                return(
+                    <div className="review">
+                        <h1>{review.display_title}</h1>
+                        <h3>{review.byline}</h3>
+                        <h3>{review.headline}</h3>
+                        <p>{review.summary_short}</p>
+                        <hr/>
+                    </div>
+                )
             })}
         </div>
     )
